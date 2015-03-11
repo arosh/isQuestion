3a8e02e6dc7b98d6ae6ffa83fe6556ba51df6553
Sho Iizuka <sho.i518@gmail.com>
2015-03-12 02:14:48 +0900

    modified get.js

--- get.js
+++ get.js
@@ -2,16 +2,34 @@
 
 var casper = require('casper').create();
 if (casper.cli.args.length < 1) {
-  casper.echo('usage: casperjs more.js BASE');
+  casper.echo('usage: casperjs get.js BASE');
   casper.exit();
 }
 var base = parseInt(casper.cli.args[0], 10);
+casper.echo('base = ' + base);
 var fs = require('fs');
-
-casper.start('http://ask.fm/EzoeRyou');
 
 var clickMax = 50;
 var progress = 0;
+
+casper.start('http://ask.fm/EzoeRyou', function() {
+  this.fill('#more-container', {
+    page: Math.max(clickMax*base - 1, 0) | 0
+  });
+  this.click('#more-container input[name=commit]');
+  this.wait(2000);
+  ++progress;
+  this.echo('progress ' + progress + '/' + clickMax);
+});
+
+for (var i = 1; i < clickMax; i++) {
+  casper.then(function() {
+    this.click('#more-container input[name=commit]');
+    this.wait(2000);
+    ++progress;
+    this.echo('progress ' + progress + '/' + clickMax);
+  });
+}
 
 var randomFileName = function() {
   var filename = null;
@@ -21,25 +39,6 @@ var randomFileName = function() {
   return filename;
 };
 
-casper.then(function() {
-  this.fill('#more-container', {
-    page: clickMax*base - 1
-  });
-  this.click('#more-container input[name=commit]');
-  this.wait(500);
-});
-
-for (var i = 0; i < clickMax; i++) {
-  casper.then(function() {
-    this.fill('#more-container', {
-      page: clickMax*base + i
-    });
-    this.click('#more-container input[name=commit]');
-    this.wait(500);
-    ++progress;
-    this.echo('progress ' + progress + '/' + clickMax);
-  });
-}
 
 casper.then(function() {
   this.page.includeJs('./jquery-2.1.3.min.js');
@@ -54,6 +53,7 @@ casper.then(function() {
     });
     return results;
   });
+  this.capture('capture.png');
   var name = randomFileName();
   fs.write(name, JSON.stringify(qa));
   this.echo('name = ' + name);
